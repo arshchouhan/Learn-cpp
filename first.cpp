3a55943b6fe587edc5bd2b443e917cd3ae23a19b
arshchouhan <arshchouhan004@gmail.com>
2024-07-03 13:53:06 +0530

    Add Hello World

---
+++ first.cpp
@@ -0,0 +1,13 @@
+// Your First C++ Program
+
+#include<iostream>
+using namespace std;
+int main() {
+    int a,b;
+    cout<<"Enter two numbers: ";
+    cin>>a>>b;
+    cout<<"First number: "<<a;
+    cout<<"Second number: "<<b;
+    return 0;
+}
+
